9e13650c7c56880832eee42702b109048253151e
Javir Urro <Javir@Javirs-iMac.local>
2017-03-30 17:30:08 -0500

    First Commit

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,123 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class PlayerController : MonoBehaviour {
+
+	[SerializeField] private float moveSpeed = 10.0f;
+	[SerializeField] private GameObject fireTrail;
+
+	private CharacterController characterController;
+	private Animator anim;
+	private bool movement = true;
+
+	private const string FIRE1 = "Fire1";
+	private const string FIRE2 = "Fire2";
+	private BoxCollider[] swordColliders;
+
+	private float vertical;
+	private float horizontal;
+
+	private ParticleSystem fireTRailParticles;
+
+
+
+
+	// Use this for initialization
+	void Start () {
+		fireTrail.SetActive (false);
+		characterController = GetComponent<CharacterController> ();	
+		anim = GetComponent<Animator> ();
+		swordColliders = GetComponentsInChildren<BoxCollider> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		vertical = Input.GetAxis ("Vertical");
+		horizontal = Input.GetAxis ("Horizontal");
+
+		if (!GameManager.instance.GameOver) {
+			Vector3 moveDirection = new Vector3(horizontal,0,vertical);
+			if (movement) {
+				characterController.SimpleMove (moveDirection * moveSpeed);
+			}
+			float animValue = Mathf.Abs(vertical) + Mathf.Abs(horizontal);
+		
+			anim.SetFloat("Forward", animValue, .1f, Time.deltaTime);
+
+			if(Input.GetAxisRaw(FIRE1)!=0){
+				anim.Play("DoubleChop");
+
+			}
+
+			if(Input.GetAxisRaw(FIRE2)!=0){
+				anim.Play("SpinAttack");
+			}
+		}
+	}
+
+	void FixedUpdate(){
+		if (!GameManager.instance.GameOver) {
+			turning ();
+		}
+	}
+
+	void turning(){
+
+		Vector3 turnDir = new Vector3 (CrossPlatformInputManager.GetAxisRaw ("Horizontal"), 0f, CrossPlatformInputManager.GetAxisRaw ("Vertical"));
+
+		if (turnDir != Vector3.zero) {
+			// Create a vector from the player to the point on the floor the raycast from the mouse hit.
+			Vector3 playerToMouse = (transform.position + turnDir) - transform.position;
+
+			// Ensure the vector is entirely along the floor plane.
+			playerToMouse.y = 0f;
+
+			// Create a quaternion (rotation) based on looking down the vector from the player to the mouse.
+			Quaternion newRotatation = Quaternion.LookRotation (playerToMouse);
+			//transform.rotation = newRotatation;
+			transform.rotation = Quaternion.Lerp(transform.rotation, newRotatation, Time.deltaTime * 10f);
+
+		}
+
+	}
+
+	public void BeginAttack(){
+		foreach (var weapon in swordColliders) {
+			weapon.enabled = true;
+		}
+
+	}
+
+	public void EndAttack(){
+		foreach (var weapon in swordColliders) {
+			weapon.enabled = false;
+		}
+	}
+
+	public void disableControl(){
+		movement = false;
+	}
+
+	public void enableControl(){
+		movement = true;
+	}
+
+	public void SpeedPowerUp(){
+		StartCoroutine (fireTrailRoutine ());
+	}
+
+	IEnumerator fireTrailRoutine(){
+		fireTrail.SetActive (true);
+		moveSpeed = 10f;
+		yield return new WaitForSeconds (10f);
+		moveSpeed = 5f;
+		fireTRailParticles = fireTrail.GetComponent<ParticleSystem> ();
+		var em = fireTRailParticles.emission;
+		em.enabled = false;
+		yield return new WaitForSeconds (3f);
+		em.enabled = true;
+		fireTrail.SetActive (false);
+	}
+}
---
+++ Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,101 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Assertions;
+
+public class PlayerHealth : MonoBehaviour {
+
+	[SerializeField] int startingHealth = 100;
+	[SerializeField] float timeSinceLastHit = 2f;
+	[SerializeField] Slider healthSlider;
+	[SerializeField] GameObject healthP;
+
+
+	private float timer = 0f;
+	private CharacterController characterController;
+	private Animator anim;
+	private int currentHealth;
+	private AudioSource audio;
+	private ParticleSystem blood;
+	private ParticleSystem aura;
+
+
+
+	public int CurrentHealth{
+		get{ 
+			return currentHealth;
+		}
+
+		set{ 
+			if (value < 0) {
+				currentHealth = 0;
+			} else {
+				currentHealth = value;
+			}
+		}
+	}
+
+	void Awake(){
+		Assert.IsNotNull(healthSlider);
+	}
+
+
+	// Use this for initialization
+	void Start () {
+		anim = GetComponent<Animator> ();
+		characterController = GetComponent<CharacterController> ();
+		currentHealth = startingHealth;
+		audio = GetComponent<AudioSource> ();
+		blood = GetComponentInChildren<ParticleSystem> ();
+		healthP.SetActive (false);
+		aura = healthP.GetComponent<ParticleSystem> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		timer += Time.deltaTime;
+	}
+
+	void OnTriggerEnter(Collider other){
+		if (timer >= timeSinceLastHit && !GameManager.instance.GameOver) {
+			if(other.CompareTag("Weapon")){
+				takeHit ();
+				blood.Play ();
+				timer = 0;
+				
+			}
+		}
+	}
+
+	void takeHit(){
+		if (currentHealth > 0) {
+			GameManager.instance.PlayerHit (currentHealth);
+			anim.Play ("Hurt");
+			currentHealth -= 10;
+			healthSlider.value = currentHealth;
+			audio.PlayOneShot (audio.clip);
+
+		}
+
+		if (currentHealth <= 0) {
+			killPlayer ();
+		}
+	}
+
+	void killPlayer(){
+		GameManager.instance.PlayerHit (currentHealth);
+		anim.SetTrigger ("HeroDie");
+		characterController.enabled = false;
+		audio.PlayOneShot (audio.clip);
+	}
+
+	public void PowerUpHealth(){
+		healthP.SetActive (true);
+		if (currentHealth <= 70) {
+			CurrentHealth += 30;
+		} else if (currentHealth < startingHealth) {
+			CurrentHealth = startingHealth;
+		}
+		healthSlider.value = CurrentHealth;
+	}
+}
